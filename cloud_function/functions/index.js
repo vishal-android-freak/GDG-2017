7e1427ba2543c1559c56b2c2c12363e2af46a335
Vishal Dubey <yzvishal.vd@gmail.com>
2018-04-15 09:55:04 +0530

    updated firebase functions and admin sdk

--- cloud_function/functions/index.js
+++ cloud_function/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 
 const ApiAiApp = require('actions-on-google').ApiAiApp;
 
@@ -14,7 +14,7 @@ exports.receiveAssistantRequests = functions.https.onRequest((request, response)
         const device = app.getArgument('devices');
         const status = app.getArgument('status');
 
-        admin.database().ref(`/automation/${device}/value`).set(status)
+        return admin.database().ref(`/automation/${device}/value`).set(status)
             .then(snapshot => {
                 app.ask(`Ok, switching ${device} ${status}. Do you want to control anything else?`);
                 response.send(200);
